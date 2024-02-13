a814ea1950116f9a481cd3be8297c13349d164b7
Anurag kumar <114020159+anuragkrgupta@users.noreply.github.com>
2024-02-14 00:26:42 +0530

    Delete practice_2.java

--- practice_2.java
+++
@@ -1,8 +0,0 @@
-public class practice_2 {
-    public static void main(String[] args) {
-        int number=12;
-        number += 5; //17
-        number %= 2; //17%2 = 1
-        System.out.println(number);
-    }
-}
